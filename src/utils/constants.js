21d6f9996622b08dd9cb1b97d4edc41c40211982
Mbehbahani <mohammad00p@gmial.com>
2024-08-13 16:11:46 +0200

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'newbackend-production-2253.up.railway.app/api/v1';
+export const PAYPAL_CLIENT_ID = 'test'
+export const SERVER_URL = 'newbackend-production-2253.up.railway.app'
